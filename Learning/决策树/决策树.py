c2b6975c6278bd0bead2276e44eaafa07cb8b7fa
Alex Liang <2499254924@qq.com>
2024-06-05 00:49:32 +0800

    决策树

---
+++ Learning/决策树/决策树.py
@@ -0,0 +1,192 @@
+#Liangyz
+#2024/6/4  23:39
+
+from matplotlib.font_manager import FontProperties
+import matplotlib.pyplot as plt
+from math import log
+import operator
+import pickle
+
+
+def createDataSet():
+    dataSet = [[0, 0, 0, 0, 'no'],  # 数据集
+               [0, 0, 0, 1, 'no'],
+               [0, 1, 0, 1, 'yes'],
+               [0, 1, 1, 0, 'yes'],
+               [0, 0, 0, 0, 'no'],
+               [1, 0, 0, 0, 'no'],
+               [1, 0, 0, 1, 'no'],
+               [1, 1, 1, 1, 'yes'],
+               [1, 0, 1, 2, 'yes'],
+               [1, 0, 1, 2, 'yes'],
+               [2, 0, 1, 2, 'yes'],
+               [2, 0, 1, 1, 'yes'],
+               [2, 1, 0, 1, 'yes'],
+               [2, 1, 0, 2, 'yes'],
+               [2, 0, 0, 0, 'no']]
+    labels = ['年龄', '有工作', '有自己的房子', '信贷情况']  # 特征标签
+    return dataSet, labels  # 返回数据集和分类属性
+
+
+def createTree(dataset,labels,featlabels):
+    classlist = [example[-1] for example in dataset]
+    if classlist.count(classlist[0]) == len(classlist):
+        return classlist[0]
+    if len(dataset[0]) == 1:
+        return majorityCnt(classlist)#当数据集中只有一个特征时，返回出现次数最多的类别
+    bestFeat = chooseBestFeatureToSplit(dataset)
+    bestFeatLabel = labels[bestFeat]
+    featlabels.append(bestFeatLabel)
+    myTree = {bestFeatLabel:{}}
+    del labels[bestFeat]
+    featValues = [example[bestFeat] for example in dataset]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataset,bestFeat,value),subLabels,featlabels)
+    return myTree
+
+
+def majorityCnt(classList):#多数表决
+    classCount = {}
+    for vote in classList:#统计classList中每个元素出现的次数
+        if vote not in classCount.keys():#如果字典classCount没有这个键
+            classCount[vote] = 0#添加这个键
+        classCount[vote] += 1#classCount[vote]记录了vote出现的次数
+    sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)#根据字典的值降序排序
+    return sortedClassCount[0][0]
+
+
+def chooseBestFeatureToSplit(dataset):
+    numFeatures = len(dataset[0]) - 1
+    baseEntropy = calcShannonEnt(dataset)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataset]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataset,i,value)
+            prob = len(subDataSet)/float(len(dataset))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+
+def splitDataSet(dataset,axis,value):
+    retDataSet = []
+    for featVec in dataset:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+def calcShannonEnt(dataset):
+    numexamples = len(dataset)
+    labelCounts = {}
+    for featVec in dataset:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numexamples
+        shannonEnt -= prob * log(prob,2)
+    return shannonEnt
+
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth:
+            maxDepth = thisDepth
+    return maxDepth
+
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    arrow_args = dict(arrowstyle='<-')
+    font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=14)
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction', xytext=centerPt,
+                            textcoords='axes fraction', va='center', ha='center', bbox=nodeType, arrowprops=arrow_args,
+                            FontProperties=font)
+
+
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString, va='center', ha='center', rotation=30)
+
+
+def plotTree(myTree, parentPt, nodeTxt):
+    decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+    leafNode = dict(boxstyle="round4", fc="0.8")
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD
+
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5 / plotTree.totalW
+    plotTree.yOff = 1.0
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.show()
+
+
+
+def main() -> None:
+    dataset, labels = createDataSet()
+    featlabels = []
+    myTree = createTree(dataset, labels, featlabels)
+    print(myTree)
+    with open('myTree.pkl', 'wb') as fw:
+        pickle.dump(myTree, fw)
+    print('决策树构建完成！')
+    print('特征标签：', featlabels)
+    createPlot(myTree)
+
+
+if __name__ == '__main__':
+    main()
