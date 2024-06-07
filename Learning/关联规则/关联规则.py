d34913fcc28bd727a47d64684cc0a0af052fa3ab
Alex Liang <2499254924@qq.com>
2024-06-07 13:41:36 +0800

    关联规则

---
+++ Learning/关联规则/关联规则.py
@@ -0,0 +1,126 @@
+#Liangyz
+#2024/6/7  0:39
+
+def loadDataSet():
+    return [[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
+
+
+def createC1(dataSet):
+    C1 = []
+    for transaction in dataSet:
+        for item in transaction:
+            if not [item] in C1:
+                C1.append([item])
+    C1.sort()
+    return list(map(frozenset, C1))
+
+
+def scanD(D, Ck, minSupport):
+    ssCnt = {}
+    for tid in D:
+        for can in Ck:
+            if can.issubset(tid):
+                if can not in ssCnt:
+                    ssCnt[can] = 1
+                else:
+                    ssCnt[can] += 1
+    numItems = float(len(list(D)))
+    retList = []
+    supportData = {}
+    for key in ssCnt:
+        support = ssCnt[key] / numItems
+        if support >= minSupport:
+            retList.insert(0, key)
+        supportData[key] = support
+    return retList, supportData
+
+
+def aprioriGen(Lk, k):
+    retList = []
+    lenLk = len(Lk)
+    for i in range(lenLk):
+        for j in range(i+1, lenLk):
+            L1 = list(Lk[i])[:k-2]
+            L2 = list(Lk[j])[:k-2]
+            L1.sort()
+            L2.sort()
+            if L1 == L2:
+                retList.append(Lk[i] | Lk[j])
+    return retList
+
+
+def apriori(dataSet, minSupport=0.5):
+    C1 = createC1(dataSet)
+    # D = list(map(set, dataSet))
+    L1, supportData = scanD(dataSet, C1, minSupport)
+    L = [L1]
+    k = 2
+    while len(L[k-2]) > 0:
+        Ck = aprioriGen(L[k-2], k)
+        Lk, supK = scanD(dataSet, Ck, minSupport)
+        if len(Lk) == 0:
+            break
+        supportData.update(supK)
+        L.append(Lk)
+        k += 1
+    return L, supportData
+
+
+def generateRules(L, supportData, minConf=0.7):
+    bigRuleList = []
+    for i in range(1, len(L)):
+        for freqSet in L[i]:
+            H1 = [frozenset([item]) for item in freqSet]
+            if i > 1:
+                rulesFromConseq(freqSet, H1, supportData, bigRuleList, minConf)
+
+
+def rulesFromConseq(freqSet, H, supportData, brl, minConf=0.7):
+    m = len(H[0])
+    while len(freqSet) > m:
+        H = calcConf(freqSet, H, supportData, brl, minConf)
+        if len(H) > 1:
+            H = aprioriGen(H, m+1)
+            m += 1
+        else:
+            break
+
+
+def calcConf(freqSet, H, supportData, brl, minConf=0.7):
+    prunedH = []
+    for conseq in H:
+        conf = supportData[freqSet] / supportData[freqSet-conseq]
+        if conf >= minConf:
+            print(freqSet-conseq, '-->', conseq, 'conf:', conf)
+            brl.append((freqSet-conseq, conseq, conf))
+            prunedH.append(conseq)
+    return prunedH
+
+
+def main() -> None:
+    dataSet = loadDataSet()
+    L, supportData = apriori(dataSet)
+    i = 0
+    for freqSet in L:
+        i+=1
+        print('第{}次迭代'.format(i))
+        print(freqSet)
+        print('---------------------------------')
+    rules = generateRules(L, supportData, minConf=0.7)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
